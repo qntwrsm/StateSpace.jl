25d6268b908622ba7a12eb8de77b0e0e84adf7fe
Quint Wiersma <qnt.wrsm@gmail.com>
2022-01-11 12:45:35 +0100

    Cleaned up forecast routines with multiple dispatch

--- StateSpace/forecast.jl
+++ StateSpace/forecast.jl
@@ -9,6 +9,47 @@ forecast.jl
 =#
 
 """
+	quad_update!(C, X, A, B, tmp)
+	
+Compute the quadratic time transition for forecast objects for time ``T+h`` for
+a linear Gaussian State Space model, with transition matrix `A` and constant
+`B`. Storing the results in `C`.
+
+#### Arguments
+  - `X::AbstractMatrix`		: input (k x k)
+  - `A::AbstractMatrix`		: transition matrix (n x k)
+  - `B::AbstractMatrix`		: constant matrix (n x n)
+  - `tmp::AbstractMatrix`	: tmp storage array (n x k)
+
+#### Returns
+  - `C::AbstractMatrix`		: forecast object (n x n)
+"""
+function quad_update!(C::AbstractMatrix, X::AbstractMatrix, A::AbstractMatrix, B::AbstractMatrix, tmp::AbstractMatrix)
+	# AｘX
+	mul!(tmp, A, X)
+	# AｘXｘA'
+	mul!(C, tmp, transpose(A))
+	# C = AｘXｘA' + B
+	@. C+= B
+end
+
+function quad_update!(C::AbstractMatrix, X::AbstractMatrix, A::Diagonal, B::AbstractMatrix, tmp::AbstractMatrix)
+	# C = AｘXｘA' + B
+	@. C= A.diag*X*transpose(A.diag) + B
+end
+
+function quad_update!(C::AbstractMatrix, X::AbstractMatrix, A::AbstractMatrix, B::Diagonal, tmp::AbstractMatrix)
+	# AｘX
+	mul!(tmp, A, X)
+	# AｘXｘA'
+	mul!(C, tmp, transpose(A))
+	# C = AｘXｘA' + B
+	@inbounds @fastmath for i in axes(B,1)
+		C[i,i]+= B.diag[i]
+	end
+end
+
+"""
 	forecast(f, mat, h)
 	
 Compute ``h``-step ahead state forecasts and corresponding variances for the
@@ -30,10 +71,6 @@ function forecast(f::Filter, mat::SysMat, h::Integer)
 	(n,p)= size(mat.Z)
 	T_len= size(f.a,2)
 	
-	# Store views of transpose
-	Tt= transpose(mat.T)
-	Zt= transpose(mat.Z)
-	
 	#Initialize tmp. cont.
 	tmp_np= Matrix{Float64}(undef, (n,p))
 	tmp_p= Matrix{Float64}(undef, (p,p))
@@ -43,30 +80,26 @@ function forecast(f::Filter, mat::SysMat, h::Integer)
 	P_h= Array{Float64,3}(undef, (p,p,h))
 	F_h= Array{Float64,3}(undef, (n,n,h))
 	
-	# Initialize forecast recursions
-	# Forecast
-	@views mul!(a_h[:,1], mat.T, f.a[:,T_len])
-	# Forecast variance
-	P_h[:,:,1]= Q
-	@views mul!(tmp_p, f.P[:,p*(T_len-1)+1:p*T_len], Tt)
-	@views mul!(P_h[:,:,1], mat.T, tmp_p, 1., 1.)
-	# Forecast error variance
-	F_h[:,:,1]= mat.H
-	@views mul!(tmp_np, mat.Z, P_h[:,:,1])
-	@views mul!(F_h[:,:,1], tmp_np, Zt, 1., 1.)
-	
 	# Loop over horizon
-	@inbounds @fastmath for j in 2:h
+	@inbounds @fastmath for j in 1:h
+		# Store views
+		a_f= view(a_h,:,j)
+		P_f= view(P_h[:,:,j])
+		if j != 1
+			a_c= view(a_h,:,j-1)
+			P_c= view(P_h[:,:,j-1])
+		else
+			a_c= view(f.a,:,T_len)
+			P_c= view(f.P,:,:,T_len)
+		end
+		F_f= view(F_h,:,:,j)
+		
 		# Forecast
-		@views mul!(a_h[:,j], mat.T, a_h[:,j-1])
+		mul!(a_f, mat.T, a_c)
 		# Forecast variance
-		P_h[:,:,j]= mat.Q
-		@views mul!(tmp_p, P_h[:,:,j-1], Tt)
-		@views mul!(P_h[:,:,j], mat.T, tmp_p, 1., 1.)
+		quad_update!(P_f, P_c, mat.T, mat.Q, tmp_p)
 		# Forecast error variance
-		F_h[:,:,j]= mat.H
-		@views mul!(tmp_np, mat.Z, P_h[:,:,j])
-		@views mul!(F_h[:,:,j], tmp_np, Zt, 1., 1.)
+		quad_update!(F_f, P_f, mat.Z, mat.H, tmp_np)
 	end
 	
 	return (a_h, P_h, F_h)
@@ -78,47 +111,38 @@ end
 Compute ``h``-step ahead state forecasts and corresponding variances for the
 latent states as well as forecast error variances for a State Space model and
 storing them in `a_h`, `P_h`, and `F_h`. See also `forecast`.
-
 """
 function forecast!(a_h::AbstractMatrix, P_h::AbstractArray, F_h::AbstractArray, 
 					f::Filter, mat::SysMat, h::Integer)
 	# Get dimensions
 	(n,p)= size(mat.Z)
-	T_len= size(k.a,2)
-	
-	# Store views of transpose
-	Tt= transpose(mat.T)
-	Zt= transpose(mat.Z)
-	
+	T_len= size(f.a,2)
+
 	#Initialize tmp. cont.
 	tmp_np= Matrix{Float64}(undef, (n,p))
 	tmp_p= Matrix{Float64}(undef, (p,p))
-	
-	# Initialize forecast recursions
-	# Forecast
-	@views mul!(a_h[:,1], mat.T, f.a[:,T_len])
-	# Forecast variance
-	P_h[:,:,1]= Q
-	@views mul!(tmp_p, f.P[:,p*(T_len-1)+1:p*T_len], Tt)
-	@views mul!(P_h[:,:,1], mat.T, tmp_p, 1., 1.)
-	# Forecast error variance
-	F_h[:,:,1]= mat.H
-	@views mul!(tmp_np, mat.Z, P_h[:,:,1])
-	@views mul!(F_h[:,:,1], tmp_np, Zt, 1., 1.)
-	
+
 	# Loop over horizon
-	@inbounds @fastmath for j in 2:h
+	@inbounds @fastmath for j in 1:h
+		# Store views
+		a_f= view(a_h,:,j)
+		P_f= view(P_h[:,:,j])
+		if j != 1
+			a_c= view(a_h,:,j-1)
+			P_c= view(P_h[:,:,j-1])
+		else
+			a_c= view(f.a,:,T_len)
+			P_c= view(f.P,:,:,T_len)
+		end
+		F_f= view(F_h,:,:,j)
+
 		# Forecast
-		@views mul!(a_h[:,j], mat.T, a_h[:,j-1])
+		mul!(a_f, mat.T, a_c)
 		# Forecast variance
-		P_h[:,:,j]= mat.Q
-		@views mul!(tmp_p, P_h[:,:,j-1], Tt)
-		@views mul!(P_h[:,:,j], mat.T, tmp_p, 1., 1.)
+		quad_update!(P_f, P_c, mat.T, mat.Q, tmp_p)
 		# Forecast error variance
-		F_h[:,:,j]= mat.H
-		@views mul!(tmp_np, mat.Z, P_h[:,:,j])
-		@views mul!(F_h[:,:,j], tmp_np, Zt, 1., 1.)
+		quad_update!(F_f, P_f, mat.Z, mat.H, tmp_np)
 	end
-	
+
 	return nothing
 end
