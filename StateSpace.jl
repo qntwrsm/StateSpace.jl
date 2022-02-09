7b67a73e95f30b996f5a25a0da40025981c115e8
Quint Wiersma <qnt.wrsm@gmail.com>
2022-02-09 12:29:13 +0100

    Add functionalityy to initialize StateSpaceSystem empty

--- StateSpace.jl
+++ StateSpace.jl
@@ -3,8 +3,9 @@ StateSpace.jl
 
     Provides methods for a linear Gaussian State Space model such as filtering
     (Kalman filter), smoothing (Kalman smoother), forecasting, likelihood
-    evaluation, and estimation of hyperparameters (Maximum Likelihood, EM, and 
-	penalized EM)
+    evaluation, and estimation of hyperparameters (Maximum Likelihood, 
+	Expectation-Maximization (EM), and Expectation-Conditional Maximization 
+	(ECM), w/ and w/o penalization)
 
 @author: Quint Wiersma <q.wiersma@vu.nl>
 
@@ -13,7 +14,7 @@ StateSpace.jl
 
 module StateSpace
 
-using LinearAlgebra
+using LinearAlgebra, Statistics
 
 export 
 # Structs
@@ -32,21 +33,41 @@ export
 # Log Likelihood
 	loglik
 	
-# Structs
-# KalmanFilter
-struct KalmanFilter end
+# Type
+abstract type KalmanFilter end
 
-# State Space model system matrices 
-struct SysMat{TZ, TT, TH, TQ, ta, TP}
+# Struct
+# State space model system
+struct StateSpaceSystem{TZ, TT, Td, Tc, TH, TQ, Ta, TP}
 	Z::TZ	# system matrix Z
 	T::TT	# system matrix T
+	d::Td	# mean adjustment observation
+	c::Tc	# mean adjustment state
 	H::TH	# system matrix H
 	Q::TQ	# system matrix Q
-	a1::TA	# initial state
+	a1::Ta	# initial state
 	P1::TP	# initial state variance
+end
+function StateSpaceSystem{TZ, TT, Td, Tc, TH, TQ, Ta, TP}(n::Integer, p::Integer) where {TZ, TT, Td, Tc, TH, TQ, Ta, TP}
+    # Initialize system components
+    Z= similar(TZ, n, p)
+    T= TT <: Diagonal ? Diagonal{eltype(TT)}(undef, p) : similar(TT, p, p)
+	d= similar(Td, n)
+	c= similar(Tc, p)
+    H= TH <: Diagonal ? Diagonal{eltype(TH)}(undef, n) : similar(TH, n, n)
+    Q= TQ <: Diagonal ? Diagonal{eltype(TQ)}(undef, p) : similar(TQ, p, p)
+	a1= similar(Ta, p)
+	P1= TP <: Diagonal ? Diagonal{eltype(TP)}(undef, p) : similar(TP, p, p)
+
+    return StateSpaceSystem(Z, T, d, c, H, Q, a1, P1)
 end
 
 # Include programs
+# Models
+include("models/state_space.jl")
+include("models/dynamic_factor.jl")
+include("models/dynamic_factor_spatial_error.jl")
+
 # Filters
 include("filters/multivariate_filter.jl")
 include("filters/univariate_filter.jl")
@@ -59,5 +80,10 @@ include("forecast.jl")
 
 # Log-likelihood
 include("loglik.jl")
+
+# Estimation
+# EM
+# include("estim/pecm.jl")
+# ML
 
 end
