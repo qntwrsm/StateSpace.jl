7c1ed0c485bc8efc4cc6d3091470d966bac391d6
Quint Wiersma <qnt.wrsm@gmail.com>
2022-03-23 15:39:53 +0100

    Cleaned main module function

--- StateSpace.jl
+++ StateSpace.jl
@@ -14,15 +14,33 @@ StateSpace.jl
 
 module StateSpace
 
-using LinearAlgebra, Statistics, ForwardDiff, Optim
+push!(LOAD_PATH, "/Users/quintwiersma/Dropbox/VU/PhD/code/jllib/ProximalMethods/")
+
+using 	LinearAlgebra, 
+		Statistics, 
+		Optim, 
+		ProximalMethods, 
+		FiniteDifferences
 
 export 
 # Structs
+	# filters and smoother 
 	MultivariateFilter, 
 	WoodburyFilter, 
 	UnivariateFilter,
 	Smoother,
-	SysMat,
+	# system
+	LinearTimeInvariant,
+	LinearTimeVariant,
+	# penalization
+	NoPen,
+	Lasso, 
+	GroupLasso,
+	SparseGroupLasso,
+	# models
+	Independent, Idiosyncratic, SpatialErrorModel,
+	NoConstant, Constant, Exogeneous,
+	DynamicFactorModel,
 # Filter
 	kalman_filter!,
 # Smoother
@@ -31,65 +49,40 @@ export
 # Forecast
 	forecast, forecast!,
 # Log Likelihood
-	loglik
+	loglik,
+# Estimation
+	maximum_likelihood!,
+	em!,
+	ecm!
 	
-# Type
+# Types
+"""
+    KalmanFilter
+
+Abstract type for Kalman filters.
+"""
 abstract type KalmanFilter end
-abstract type Penalization end
 
-# Struct
-# State space model system
-struct StateSpaceSystem{TZ, TT, Td, Tc, TH, TQ, Ta, TP}
-	Z::TZ	# system matrix Z
-	T::TT	# system matrix T
-	d::Td	# mean adjustment observation
-	c::Tc	# mean adjustment state
-	H::TH	# system matrix H
-	Q::TQ	# system matrix Q
-	a1::Ta	# initial state
-	P1::TP	# initial state variance
-end
-# Constructor
-function StateSpaceSystem{TZ, TT, Td, Tc, TH, TQ, Ta, TP}(n::Integer, p::Integer) where {TZ, TT, Td, Tc, TH, TQ, Ta, TP}
-    # Initialize system components
-    Z= similar(TZ, n, p)
-    T= TT <: Diagonal ? Diagonal{eltype(TT)}(undef, p) : similar(TT, p, p)
-	d= similar(Td, n)
-	c= similar(Tc, p)
-    H= TH <: Diagonal ? Diagonal{eltype(TH)}(undef, n) : similar(TH, n, n)
-    Q= TQ <: Diagonal ? Diagonal{eltype(TQ)}(undef, p) : similar(TQ, p, p)
-	a1= similar(Ta, p)
-	P1= TP <: Diagonal ? Diagonal{eltype(TP)}(undef, p) : similar(TP, p, p)
+"""
+    KalmanSmoother
 
-    return StateSpaceSystem(Z, T, d, c, H, Q, a1, P1)
-end
+Abstract type for Kalman smoothers.
+"""
+abstract type KalmanSmoother end
 
-# Penalization
-struct Lasso{Tγ, Tw} <: Penalization
-	γ::Tγ		# penalization strength
-	weights::Tw	# weights
-end
-struct GroupLasso{Tγ, Tw, Tg} <: Penalization
-	γ::Tγ		# penalization strength
-	weights::Tw	# weights
-	groups::Tg	# group structure
-end
-struct SparseGroupLasso{Tγ, Tw, Tg} <: Penalization
-	γ::Tγ			# penalization strength
-	α::Tγ			# mixing parameter
-	weights_l1::Tw	# weights ℓ₁-norm
-	weights_l2::Tw	# weights ℓ₂-norm
-	groups::Tg		# group structure
-end
+"""
+    StateSpaceModel
+
+Abstract type for state space models.
+"""
+abstract type StateSpaceModel end
+
+# pca
+include("../misc/stats/pca.jl")
 
 # Include programs
-# Models
-include("models/state_space.jl")
-# Dynamic factor models
-include("models/dynamic_factor.jl")
-include("models/sparse_dynamic_factor.jl")
-include("models/dynamic_factor_spatial_error.jl")
-include("models/sparse_dynamic_factor_spatial_error.jl")
+# System
+include("system.jl")
 
 # Filters
 include("filters/multivariate_filter.jl")
@@ -105,10 +98,19 @@ include("forecast.jl")
 include("loglik.jl")
 
 # Estimation
+include("estim/estim.jl")
+# ML
+include("estim/ml/ml.jl")
 # EM
 include("estim/em/em.jl")
 include("estim/em/mstep.jl")
 include("estim/em/estep.jl")
-# ML
+
+# Models
+include("models/state_space.jl")
+# Dynamic factor models
+include("models/error_model.jl")
+include("models/mean_model.jl")
+include("models/dynamic_factor_model.jl")
 
 end
