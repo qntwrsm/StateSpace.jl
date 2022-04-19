d364ce521f6e2523cfc75e774cc6e9ec3df8b99b
Quint Wiersma <qnt.wrsm@gmail.com>
2022-04-19 16:43:28 +0200

    Adjusted univariate filter in the same way as the multivariate filters

--- src/filters/univariate_filter.jl
+++ src/filters/univariate_filter.jl
@@ -10,12 +10,27 @@ univariate_filter.jl
 =#
 
 # Struct
-struct UnivariateFilter{Ta, TP, Tv, TF, TK} <: KalmanFilter
+struct UnivariateFilter{Ta, TP, Tv, TF, TK, Tm} <: KalmanFilter
 	a::Ta   # filtered state
 	P::TP   # filtered state variance
 	v::Tv   # forecast error
 	F::TF	# forecast error variance
 	K::TK	# Kalman gain
+    tmp::Tm # buffer (p x p)
+end
+# Constructor
+function UnivariateFilter(n::Integer, p::Integer, T_len::Integer, T::Type)
+    # filter output
+    a= Array{T,3}(undef, p, n+1, T_len)
+    P= Array{T,4}(undef, p, p, n+1, T_len)
+    v= Matrix{T}(undef, n, T_len)
+    F= Matrix{T}(undef, n, T_len)
+    K= Array{T,3}(undef, p, n, T_len)
+
+    # buffer
+    tmp= Matrix{T}(undef, p, p)
+
+    return UnivariateFilter(a, P, v, F, K, tmp)
 end
 
 """
@@ -115,6 +130,94 @@ function predict!(a_p::AbstractVector, P_p::AbstractMatrix, a_t::AbstractVector,
 end
 
 """
+    update_filter!(filter, y, Z, d, σ², i, t)
+
+Update Kalman filter components at time `t`, storing the results in `filter`.
+
+#### Arguments
+  - `y::Real`           : data
+  - `Z::AbstractVector` : system matrix ``Z``
+  - `d::Real`           : mean adjustment observation
+  - `σ²::Real`          : observation equation variance
+  - `i::Integer`        : series
+  - `t::Integer`        : time
+
+#### Returns
+  - `filter::UnivariateFilter`  : Kalman filter components
+"""
+function update_filter!(filter::UnivariateFilter, 
+                        y::Real, 
+                        Z::AbstractVector, 
+                        d::Real, 
+                        σ²::Real, 
+                        i::Integer,
+                        t::Integer)
+    # Store views 
+    a= view(filter.a,:,i,t)
+    a_f= view(filter.a,:,i+1,t)
+    P= view(filter.P,:,:,i,t)
+    P_f= view(filter.P,:,:,i+1,t)
+    K= view(filter.K,:,i,t)
+
+    # Forecast error
+    filter.v[i,t]= y - dot(Z, a) - d
+
+    # Forecast error variance
+    filter.F[i,t]= dot(Z, P, Z) + σ²
+
+    # Kalman gain
+    mul!(K, P, Z, inv(filter.F[i,t]), .0)
+
+    # Move states and variances forward
+    forward!(a_f, P_f, a, P, K, filter.v[i,t], filter.F[i,t])
+
+    return nothing
+end
+
+"""
+    update_filter!(filter, Z, σ², i, t)
+
+Update Kalman filter components at time `t` when observation ``y`` is missing,
+i.e. NaN, storing the results in `filter`.
+
+#### Arguments
+  - `Z::AbstractVector` : system matrix ``Z``
+  - `σ²::Real`          : observation equation variance
+  - `i::Integer`        : series
+  - `t::Integer`        : time
+
+#### Returns
+  - `filter::KalmanFilter`  : Kalman filter components
+"""
+function update_filter!(filter::UnivariateFilter, 
+                        Z::AbstractVector, 
+                        σ²::Real, 
+                        i::Integer,
+                        t::Integer)
+    # Store views 
+    a= view(filter.a,:,i,t)
+    a_f= view(filter.a,:,i+1,t)
+    P= view(filter.P,:,:,i,t)
+    P_f= view(filter.P,:,:,i+1,t)
+    K= view(filter.K,:,i,t)
+
+    # Forecast error
+    filter.v[i,t]= NaN
+
+    # Forecast error variance
+    filter.F[i,t]= dot(Z, P, Z) + σ²
+
+    # Kalman gain
+    K.= zero(eltype(K))
+
+    # Move states and variances forward
+    a_f.= a
+    P_f.= P
+
+    return nothing
+end
+
+"""
 	kalman_filter!(filter, sys) 
 	
 Compute predicted states ``a`` and forecast errors ``v`` with corresponding
@@ -131,40 +234,23 @@ univariate version of the Kalman filter, storing the results in `filter`.
 function kalman_filter!(filter::UnivariateFilter, sys::LinearTimeInvariant)
 	# Get dims
 	(n,T_len)= size(sys.y)
-	p= length(sys.a1)
-	
-    # Initialize temp. containers
-    tmp_p= Matrix{Float64}(undef, (p,p))
 	
 	# Initialize filter
 	filter.a[:,1,1]= sys.a1
 	filter.P[:,:,1,1]= sys.P1
 	
-	# Tranpose
-	Zt= transpose(sys.Z)
-	
 	# Filter
 	@inbounds @fastmath for t in 1:T_len
 		for i in 1:n
-	        # Store views 
-	        a_it= view(filter.a,:,i,t)
-			a_f= view(filter.a,:,i+1,t)
-			P_it= view(filter.P,:,:,i,t)
-			P_f= view(filter.P,:,:,i+1,t)
-	        K_it= view(filter.K,:,i,t)
-			Z_i= view(Zt,:,i)
-		
-			# Forecast error
-			filter.v[i,t]= sys.y[i,t] - dot(Z_i, a_it) - sys.d[i]
-		
-			# Forecast error variance
-			filter.F[i,t]= dot(Z_i, P_it, Z_i) + sys.H.diag[i]
-		
-			# Kalman gain
-			mul!(K_it, P_it, Z_i, inv(filter.F[i,t]), .0)
-		
-			# Move states and variances forward
-			forward!(a_f, P_f, a_it, P_it, K_it, filter.v[i,t], filter.F[i,t])
+	        # Store view
+			Z_i= view(Z,i,:)
+
+            # update filter
+            if isnan(sys.y[i,t])
+                update_filter!(filter, Z_i, sys.H.diag[i], i, t)
+            else
+                update_filter!(filter, sys.y[i,t], Z_i, sys.d[i], sys.H.diag[i], i, t)
+            end
 		end
 		
 		if t < T_len
@@ -185,10 +271,6 @@ end
 function kalman_filter!(filter::UnivariateFilter, sys::LinearTimeVariant)
 	# Get dims
 	(n,T_len)= size(sys.y)
-	p= length(sys.a1)
-	
-    # Initialize temp. containers
-    tmp_p= Matrix{Float64}(undef, (p,p))
 	
 	# Initialize filter
 	filter.a[:,1,1]= sys.a1
@@ -199,27 +281,17 @@ function kalman_filter!(filter::UnivariateFilter, sys::LinearTimeVariant)
 		# store
 		Z_t= sys.Z[t]
 		H_t= sys.H[t]
-		c_t= view(sys.c,:,t)
+		
 		for i in 1:n
-	        # Store views 
-	        a_it= view(filter.a,:,i,t)
-			a_f= view(filter.a,:,i+1,t)
-			P_it= view(filter.P,:,:,i,t)
-			P_f= view(filter.P,:,:,i+1,t)
-	        K_it= view(filter.K,:,i,t)
+	        # Store view
 			Z_it= view(Z_t,i,:)
 		
-			# Forecast error
-			filter.v[i,t]= sys.y[i,t] - dot(Z_it, a_it) - sys.d[i,t]
-		
-			# Forecast error variance
-			filter.F[i,t]= dot(Z_it, P_it, Z_it) + H_t.diag[i]
-		
-			# Kalman gain
-			mul!(K_it, P_it, Z_it, inv(filter.F[i,t]), .0)
-		
-			# Move states and variances forward
-			forward!(a_f, P_f, a_it, P_it, K_it, filter.v[i,t], filter.F[i,t])
+			# update filter
+            if isnan(sys.y[i,t])
+                update_filter!(filter, Z_it, H_t.diag[i], i, t)
+            else
+                update_filter!(filter, sys.y[i,t], Z_it, sys.d[i,t], H_t.diag[i], i, t)
+            end
 		end
 		
 		if t < T_len
@@ -228,6 +300,7 @@ function kalman_filter!(filter::UnivariateFilter, sys::LinearTimeVariant)
 			P_f= view(filter.P,:,:,n+1,t)
 	        a_p= view(filter.a,:,1,t+1)
 			P_p= view(filter.P,:,:,1,t+1)
+            c_t= view(sys.c,:,t)
 			
 			# Predict states and variances
 			predict!(a_p, P_p, a_f, P_f, sys.T[t], sys.Q[t], c_t, tmp_p)
