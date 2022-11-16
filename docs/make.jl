fab95656fa458402fa01015d979c789e08409c21
Quint Wiersma <qnt.wrsm@gmail.com>
2022-11-16 17:38:31 +0100

    Update make.jl with page structure

--- docs/make.jl
+++ docs/make.jl
@@ -4,6 +4,13 @@ using Documenter, StateSpace
 makedocs(
     sitename="StateSpace.jl",
     modules=[StateSpace],
+    pages = [
+        "index.md",
+        "models.md",
+        "filter.md",
+        "smoother.md",
+        "estimation.md"
+    ]
     format = Documenter.HTML(
         prettyurls = get(ENV, "CI", nothing) == "true"
     )
